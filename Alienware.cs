8cbb1cb9baf2892e38b8e27461629aa446c0c678
Jeffrey Hermo <you@example.comjbhermo@yahoo.com>
2021-11-30 18:54:53 -0500

    Add project files.

---
+++ Alienware.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    class Alienware : Laptop
+    {
+        public override void specialServices()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Laptop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    abstract class Laptop : ILaptop
+    {
+        public string Brand { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Model { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public abstract void specialServices();
+    }
+}
